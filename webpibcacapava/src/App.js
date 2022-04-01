ee91c016006d2f536763ca44697f6f42ca723a96
lordars <adrian.r.scalia@gmail.com>
2022-03-31 23:18:54 -0300

    Secount merge

--- webpibcacapava/src/App.js
+++ webpibcacapava/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack direction="row" spacing={2}>
+      <Button><AccountCircleIcon/>Primary</Button>
+      <Button disabled>Disabled</Button>
+      <Button href="#text-buttons">Link</Button>
+    </Stack>
   );
 }
 
-export default App;
+
