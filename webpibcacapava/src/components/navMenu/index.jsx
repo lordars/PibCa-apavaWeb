22976b5b964a3157292c88b8126336bed929de2d
lordars <adrian.r.scalia@gmail.com>
2022-04-02 18:41:34 -0300

    puss foto na navcom avatar

--- webpibcacapava/src/components/navMenu/index.jsx
+++ webpibcacapava/src/components/navMenu/index.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom"
+import "./styless.css"
+import Avatar from "../avatar/avatar"
 
 export default class NavMenu extends Component {
   render() {
     return (
     <>
-    
-    <nav>
+    <nav className='NavBar'>
+        <Avatar />
         <Link  to="/"> Home</Link>
         <Link  to="/about">Sobre</Link>
       </nav>
